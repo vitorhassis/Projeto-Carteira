cfc2dd65c96e7ac2ae22123043091482192fe02a
vitorhassis <vitorassis997@gmail.com>
2025-10-30 01:38:46 -0300

    riada classe Transacao e AppDbContext com mapeamento inicial do EF Core

---
+++ CarteiraSimples/CarteiraSimples/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CarteiraSimples.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarteiraSimples.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Transacao> Transacoes => Set<Transacao>();
+    }
+
+}
---
+++ CarteiraSimples/CarteiraSimples/Models/Transacao.cs
@@ -0,0 +1,11 @@
+﻿namespace CarteiraSimples.Models
+{
+    public class Transacao
+    {
+        public int Id { get; set; }
+        public double Valor { get; set; }
+        public string Descricao { get; set; }
+        public string Tipo { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
